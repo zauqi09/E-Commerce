adab4b0c2a9932734d1bdff5c070d19770ba106e
Fuad Zauqi Nur <zauqi09@gmail.com>
2020-10-31 10:46:22 +0700

    nambah page payment

--- src/pages/pembayaran/index.js
+++ src/pages/pembayaran/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Container, Row, Col, FormControl, FormLabel } from 'react-bootstrap';
+import { connect } from "react-redux"
 
 class Pembayaran extends Component {
     constructor(props) {
@@ -6,11 +8,58 @@ class Pembayaran extends Component {
         this.state = {  }
     }
     render() { 
+        const prod = this.props.listProduct
         return ( 
             <>
+            <Container>
+                <h4>Pembayaran</h4>
+                <Row>
+                    <Col>
+                    <FormLabel>Judul</FormLabel>
+                        <FormControl placeholder={prod[2].judul} readOnly></FormControl>
+                        <FormLabel>Kategori</FormLabel>
+                        <FormControl placeholder={prod[2].kategori} readOnly></FormControl>
+                        <FormLabel>Harga</FormLabel>
+                        <FormControl placeholder={prod[2].harga} readOnly></FormControl>
+                        <FormLabel>Penulis</FormLabel>
+                        <FormControl placeholder={prod[2].penulis} readOnly></FormControl>
+                        <FormLabel>Penerbit</FormLabel>
+                        <FormControl placeholder={prod[2].penerbit} readOnly></FormControl>
+                        <FormLabel>Negara</FormLabel>
+                        <FormControl placeholder={prod[2].negara} readOnly></FormControl>
+                        <FormLabel>Bahasa</FormLabel>
+                        <FormControl placeholder={prod[2].bahasa} readOnly></FormControl>
+                        <FormLabel>Jumlah Halaman</FormLabel>
+                        <FormControl placeholder={prod[2].jumlahhal} readOnly></FormControl>
+                        <FormLabel>Cover</FormLabel>
+                        <br/>
+                        <img className="coverBook" src={prod[2].cover} alt="cover"/>
+                        <br/>
+                        <FormLabel>Deskripsi</FormLabel>
+                        <FormControl placeholder={prod[2].deskripsi} readOnly></FormControl>
+                    </Col>
+                    <Col>
+                    </Col>
+                    <Col>
+
+                    </Col>
+                </Row>
+            </Container>
             </>
          );
     }
 }
- 
-export default Pembayaran;
+const mapStateToProps = (state) => {
+    return {
+        listProduct: state.product.listProduct,
+        userList : state.auth.userListFromApp,
+        dataLogin : state.auth.dataLogin,
+    }
+    
+}
+
+const mapDispatchToProps = (dispatch) => ({
+    addProduct: (listProduct,userList,dataLogin) => dispatch({ type: "ADDPRODUCT", payload: {listProduct,userList,dataLogin}}),
+})
+
+export default connect(mapStateToProps,mapDispatchToProps)(Pembayaran)
