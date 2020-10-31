6f1409d77e9c7834419feb93536d77d8ed075daa
Irene N <32290296+rxnvz@users.noreply.github.com>
2020-10-31 18:03:44 +0700

    catalog search by title

--- src/pages/katalog/index.js
+++ src/pages/katalog/index.js
@@ -1,163 +1,92 @@
 import React, { Component } from 'react';
-import { Container,Col,Row,Card, Button, Form ,FormControl} from 'react-bootstrap';
-import carousel1 from '../../img/carousel1.jpg'
-import carousel2 from '../../img/carousel2.jpg'
-import carousel3 from '../../img/carousel3.jpg'
+import { connect } from "react-redux"
+import { Container,Col,Row,Card, Button, Form ,FormControl} from 'react-bootstrap'
 
 class Katalog extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {
+            cari: "",
+            kategori: ""
+        }
     }
+
+    onChangeInput = e => {
+        this.setState({        
+            [e.target.name]:e.target.value
+        })
+    }
+
+    // onSearch = () => {
+    //     const { cari, kategori } = this.state
+
+    //     const hasilCari = this.props.listProduct.filter(book =>{
+    //         if (kategori === "All" && cari === "" ) {
+    //             return book
+    //         } else if (book.judul.toLowerCase().includes(cari.toLowerCase()) || book.kategori === kategori) {
+    //             return book
+    //         }
+    //     })
+    // }
+
     render() { 
+        const buku = this.props.listProduct
+        const { cari, kategori } = this.state
+
         return ( 
             <Container>
                 <hr/>
                 <h3>Katalog</h3>
                 <hr/>
                 <Row>
-                    <Col sm={4}>
-                        <h4>Filter</h4>
+                    <Col sm={4} >
+                        <h4>Pencarian</h4>
                         <Form>
-                            <FormControl type="text" placeholder="Nama Buku" className="formcontrol"></FormControl>
+                            <FormControl type="text" placeholder="Nama Buku" className="formcontrol" name="cari" onChange={this.onChangeInput}></FormControl>
                             <Form.Group controlId="exampleForm.ControlSelect1">
                                 <Form.Label>Kategori</Form.Label>
-                                <Form.Control as="select">
-                                <option>Komputer</option>
-                                <option>Olahraga</option>
-                                <option>Sejarah</option>
-                                <option>Psikologi</option>
-                                <option>Novel</option>
+                                <Form.Control as="select" name="kategori" onChange={this.onChangeInput}>
+                                    <option value="Novel">Novel</option>
+                                    <option value="Sains">Sains</option>
+                                    <option value="Motivasi">Motivasi</option>
+                                    <option value="Bisnis">Bisnis</option>
+                                    <option value="Anak">Anak</option>
                                 </Form.Control>
-                            </Form.Group>
-                                
-                            <Button className="btnLogin" variant="btn btn-primary btn-block">Filter</Button>
+                                <br></br>
+                                <Button variant="btn btn-primary btn-block">Filter</Button>
+                            </Form.Group>       
                         </Form>
                     </Col>
-                    <Col sm={2}>
-                            <Card style={{ width: '9rem', marginBottom: '15px' }}>
-                                <Card.Img variant="top" src={carousel1} />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Button variant="primary btn-block">Rp. 60.000</Button>
-                                </Card.Body>
-                            </Card>
-                            <Card style={{ width: '9rem', marginBottom: '15px'}}>
-                                <Card.Img variant="top" src={carousel1} />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Button variant="primary btn-block">Rp. 60.000</Button>
-                                </Card.Body>
-                            </Card>
-                            <Card style={{ width: '9rem', marginBottom: '15px'}}>
-                                <Card.Img variant="top" src={carousel1} />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Button variant="primary btn-block">Rp. 60.000</Button>
-                                </Card.Body>
-                            </Card>
-                            <Card style={{ width: '9rem', marginBottom: '15px'}}>
-                                <Card.Img variant="top" src={carousel1} />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Button variant="primary btn-block">Rp. 60.000</Button>
-                                </Card.Body>
-                            </Card>
-                    </Col>
-                    <Col sm={2}>
-                            <Card style={{ width: '9rem', marginBottom: '15px' }}>
-                                <Card.Img variant="top" src={carousel2} />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Button variant="primary btn-block">Rp. 70.000</Button>
-                                </Card.Body>
-                            </Card>
-                            <Card style={{ width: '9rem', marginBottom: '15px' }}>
-                                <Card.Img variant="top" src={carousel2} />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Button variant="primary btn-block">Rp. 70.000</Button>
-                                </Card.Body>
-                            </Card>
-                            <Card style={{ width: '9rem', marginBottom: '15px' }}>
-                                <Card.Img variant="top" src={carousel2} />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Button variant="primary btn-block">Rp. 70.000</Button>
-                                </Card.Body>
-                            </Card>
-                            <Card style={{ width: '9rem', marginBottom: '15px' }}>
-                                <Card.Img variant="top" src={carousel2} />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Button variant="primary btn-block">Rp. 70.000</Button>
-                                </Card.Body>
-                            </Card>
-                    </Col>
-                    <Col sm={2}>
-                            <Card style={{ width: '9rem', marginBottom: '15px' }}>
-                                <Card.Img variant="top" src={carousel3} />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Button variant="primary btn-block">Rp. 66.000</Button>
-                                </Card.Body>
-                            </Card>
-                            <Card style={{ width: '9rem', marginBottom: '15px' }}>
-                                <Card.Img variant="top" src={carousel2} />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Button variant="primary btn-block">Rp. 70.000</Button>
-                                </Card.Body>
-                            </Card>
-                            <Card style={{ width: '9rem', marginBottom: '15px' }}>
-                                <Card.Img variant="top" src={carousel2} />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Button variant="primary btn-block">Rp. 70.000</Button>
-                                </Card.Body>
-                            </Card>
-                            <Card style={{ width: '9rem', marginBottom: '15px' }}>
-                                <Card.Img variant="top" src={carousel2} />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Button variant="primary btn-block">Rp. 70.000</Button>
-                                </Card.Body>
-                            </Card>
-                    </Col>
-                    <Col sm={2}>
-                            <Card style={{ width: '9rem', marginBottom: '15px' }}>
-                                <Card.Img variant="top" src={carousel3} />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Button variant="primary btn-block">Rp. 66.000</Button>
-                                </Card.Body>
-                            </Card>
-                            <Card style={{ width: '9rem', marginBottom: '15px' }}>
-                                <Card.Img variant="top" src={carousel2} />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Button variant="primary btn-block">Rp. 70.000</Button>
-                                </Card.Body>
-                            </Card>
-                            <Card style={{ width: '9rem', marginBottom: '15px' }}>
-                                <Card.Img variant="top" src={carousel2} />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Button variant="primary btn-block">Rp. 70.000</Button>
-                                </Card.Body>
-                            </Card>
-                            <Card style={{ width: '9rem', marginBottom: '15px' }}>
-                                <Card.Img variant="top" src={carousel2} />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Button variant="primary btn-block">Rp. 70.000</Button>
-                                </Card.Body>
-                            </Card>
+                    <Col sm={8} style={{display:'flex', flexWrap:'wrap'}}>
+                        {
+                        buku.filter(book =>{
+                            if (kategori === "All" && cari === "" ) {
+                                return book
+                            } else if (book.judul.toLowerCase().includes(cari.toLowerCase()) || book.kategori === kategori) {
+                                return book
+                            }
+                        }).map((book, idx) => {
+                            return  <Card key={idx} style={{ width: '9rem', marginBottom: '15px', marginRight: '15px' }}>
+                                        <Card.Img variant="top" src={book.cover} />
+                                        <Card.Body>
+                                            <Card.Title>{book.judul}</Card.Title>
+                                            <Button variant="primary btn-block">Rp {book.harga}</Button>
+                                        </Card.Body>
+                                    </Card>
+                        })
+                        }
                     </Col>
                 </Row>
             </Container>
         );
     }
 }
+
+const mapStateToProps = (state) => {
+    return {
+        listProduct: state.product.listProduct,
+    }
+    
+}
  
-export default Katalog;
+export default connect(mapStateToProps)(Katalog);
